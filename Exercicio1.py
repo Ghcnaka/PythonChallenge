80fee16e7864d451764fc94f57a44be0bc57f085
Gustavo Nakagawa <ghc.naka@gmail.com>
2020-02-21 16:14:40 -0300

    Prints, sem comentários

--- Exercicio1.py
+++ Exercicio1.py
@@ -4,13 +4,17 @@ from random import *
 def throw_dices():
     double = False
     a = randint(1,6)
+    print(f"Dado N° 1: {a}")
     b = randint(1,6)
+    print(f"Dado N° 2: {b}")
     if a == b:
+        print("Dois dados iguais!")
         double = True
     return a+b, double
 
 
 def player_move():
+    print("Jogue os dados")
     double = True
     index = 0
     movement = 0
@@ -19,19 +23,27 @@ def player_move():
         movement += mov
         index += 1
         if not double:
+            print(f"Jogada concluída! Por favor mova {movement} casas")
             return movement, False, index
+        print("Uma dupla! Com isso, você tem direito a mais uma jogada de dados")
+    print("Uh oh... Você tirou três duplas! Vá para a prisão!!")
     return 0, True, index
 
 
 def prison_escape(money):
+    print("Você está na prisão. Para sair, consiga uma dupla nos dados. Você tem 3 tentativas.")
     for index in range(0,3):
+        print(f"Tentativa N° {index+1}:")
         movement, double = throw_dices()
         if double:
+            print(f"Parabéns, uma dupla!! Saia da prisão e ande {movement} casas! Mas antes, pague 50")
             return movement, False, money - 50
+    print("Nenhuma dupla... Tente novamente na próxima rodada")
     return 0, True, money
 
 
 def default_move(piece, prison_try, money):
+    print(f"É a vez de {piece}")
     if not prison_try:
         play = player_move()
         prison_try = play[1]
@@ -42,4 +54,5 @@ def default_move(piece, prison_try, money):
     return piece, prison_try, money, play[0]
 
 
-print(default_move('Teste', False, 500))
+print(default_move("Teste Prisioneiro", True, 500))
+print(default_move("Teste Standard", False, 500))
