e04bd3ea040834af4888a56e30b29acd9d625ca0
Gustavo Nakagawa <ghc.naka@gmail.com>
2020-02-21 21:07:16 -0300

    Exercício 3 Lógica Completa (Corrigida)
    Anterior tinha interpretação errada

--- Exercicio3.py
+++ Exercicio3.py
@@ -8,12 +8,44 @@ def verify(string):
     return count
 
 
-
-def question_mark(string):
+def verify_interval(string):
     question = False
     if verify(string) == 3:
         question = True
     return question
 
+
+def question_mark(string):
+    first_num = -1
+    second_num = -1
+    count = -1
+    first_index = 0
+    boolean = True
+    for index in string:
+        count += 1
+        if not boolean:
+            break
+        try:
+            if first_num == -1:
+                first_num = int(index)
+                first_index = count + 1
+            elif second_num == -1:
+                second_num = int(index)
+            if first_num + second_num >= 10:
+                boolean = verify_interval(string[first_index:count])
+                if not boolean:
+                    break
+            if second_num != -1:
+                first_num = second_num
+                second_num = -1
+                first_index = count + 1
+        except ValueError or TypeError:
+            True
+
+    return boolean
+
+
 if question_mark(input()):
     print("OK")
+else:
+    print("not OK")
